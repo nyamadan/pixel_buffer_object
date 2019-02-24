d50221cbc3aa9e16c14cf9633354ecdf73508827
Yoshiki YAMADA <nyamadandan@gmail.com>
2019-02-24 23:56:04 +0900

    setup

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import "!style-loader!css-loader?module=false!./index.css";
+
+import * as twgl from "twgl.js";
+
+const createContext = (width: number, height: number) => {
+  const canvas = document.createElement("canvas") as HTMLCanvasElement;
+  const gl = canvas.getContext("webgl2") as WebGL2RenderingContext;
+  canvas.width = width;
+  canvas.height = height;
+  return gl;
+};
+
+const clear = (gl: WebGL2RenderingContext) => {
+    gl.clearColor(0.5, 0.5, 0.5, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+};
+
+const createApp = () => {
+  const gl = createContext(512, 512);
+
+  const render = (t: number) => {
+    clear(gl);
+    requestAnimationFrame(render);
+  };
+
+  requestAnimationFrame(render);
+
+  return gl.canvas;
+}
+
+document.body.appendChild(createApp());
+
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const options = {
+  mode: process.env["NODE_ENV"] || "development",
+  devtool: false,
+  entry: "./src/index.ts",
+  target: "web",
+  output: {
+    filename: "index.js",
+    path: __dirname + "/dist"
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      {
+        test: /\.css$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader", // translates CSS into CommonJS
+          options: {
+            sourceMap: process.env["NODE_ENV"] != "production",
+            modules: true,
+          }
+        }]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Pixel Buffer Object Test"
+    })
+  ]
+};
+
+
+if (options.mode !== "production") {
+  options.devtool = "source-map";
+  options.module.rules.push({
+    enforce: "pre",
+    test: /\.js$/,
+    loader: "source-map-loader"
+  });
+}
+
+module.exports = options;
